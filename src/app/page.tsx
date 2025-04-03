698c9783909d386d7fc4c03d354e1d7c16af8e03
prame BT <pramebt@gmail.com>
2025-04-03 17:23:46 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
